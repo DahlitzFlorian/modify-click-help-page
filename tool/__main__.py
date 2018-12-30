dd3bfef7c848d8be3f2eac1d4c7227907472dd5b
Florian Dahlitz <florian.dahlitz@ibm.com>
2018-12-30 12:48:13 +0100

    feat: created basic command-line tool

---
+++ tool/__main__.py
@@ -0,0 +1,34 @@
+import click
+
+from tool import __version__
+
+
+@click.group()
+@click.version_option(
+    __version__, "-V", "--version", message="%(prog)s, version %(version)s"
+)
+@click.pass_context
+def cli(ctx):
+    """
+    Welcome to the Help Page of tool.
+
+    $ tool ...
+    """
+    pass
+
+
+@cli.command()
+@click.option(
+    "-t",
+    "--text",
+    default="Hello World",
+    help="Text to print"
+)
+@click.pass_context
+def do(ctx, text):
+    """Print text."""
+    print(text)
+
+
+if __name__ == "__main__":
+    cli()
